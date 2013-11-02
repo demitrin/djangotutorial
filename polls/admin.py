c2c7b9f7a846f9fa808bdc9800dbe545ca60a90b
demitrin <demitri@mit.edu>
2013-11-02 11:46:02 -0700

    added search functionality to the admin paage

--- polls/admin.py
+++ polls/admin.py
@@ -1,16 +1,19 @@
 from django.contrib import admin
 from polls.models import Choice, Poll
 
-class ChoiceInLine(admin.StackedInLine):
-  model = Choice
-  extra = 3
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 class PollAdmin(admin.ModelAdmin):
-  fieldsets = [
-      (None, {'fields': ['question']}),
-      ('Date information', {'fields' : ['pub_date'], 'classes' : ['collapse']}),    
-  ]
-
-  inlines = [ChoiceInLine]
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question']
+    date_heirarchy = 'pub_date'
 
 admin.site.register(Poll, PollAdmin)
